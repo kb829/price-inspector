29c7ef1f2a3ad29d7dcc2f0f96c17cf7482c0f49
biny <kbpark829@gmail.com>
2021-06-03 20:40:57 +0900

    app.module update

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'client/public/'),
+    }),
+    ConfigModule.forRoot(), UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
