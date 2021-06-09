06daad45c92a1847b1d840cabb3886bdf90ad57a
biny <kbpark829@gmail.com>
2021-06-09 20:40:41 +0900

    edit api GET

--- server/src/config/steam-api/steam-api.service.ts
+++ server/src/config/steam-api/steam-api.service.ts
@@ -1,14 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
 import { User } from '../../user/user'
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SteamApiService {
+    constructor(private httpService: HttpService, private configService: ConfigService) {}
     private readonly user: User = {
-        userID: 'test1',
+        userID: '76561197960435530',
     }
 
+    private readonly STEAM_API_KEY = this.configService.get<string>('STEAM_API_KEY');
+
     findUser(userID: string): User{
-        let url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+'STEAM_API_KEY'+'&steamids='+userID;
+        let url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+ this.STEAM_API_KEY +'&steamids='+userID;
+
+        const ret = this.httpService.get(url);
 
         // need api call
         // if(){
