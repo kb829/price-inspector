5b30c9d59ded00fa296972310e1faba30e24c400
biny <kbpark829@gmail.com>
2021-06-23 19:12:10 +0900

    comment out some components

--- client/src/components/core/AppBar.vue
+++ client/src/components/core/AppBar.vue
@@ -1,16 +1,19 @@
 <template>
     <v-app-bar app>
         <v-app-bar-nav-icon @click="drawNav"></v-app-bar-nav-icon>
-        <v-toolbar-title>toolbar title</v-toolbar-title>
+        <v-app-bar-title>toolbar title</v-app-bar-title>
 
         <v-responsive max-width="300">
-            <v-text-field
+            <!-- <v-text-field
                 dense
                 flat
                 hide-details
                 rounded
                 solo-inverted
-            ></v-text-field>
+            ></v-text-field> -->
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
         </v-responsive>
     </v-app-bar>
 </template>
--- client/src/components/core/Navigation.vue
+++ client/src/components/core/Navigation.vue
@@ -5,9 +5,9 @@
   >
     <v-sheet
     >
-        <v-avater
+        <!-- <v-avater
         >
-        </v-avater>
+        </v-avater> -->
         <div></div>
     </v-sheet>
     <v-divider></v-divider>
@@ -15,20 +15,24 @@
         nav
         dense
     >
-        <v-list-item-group
-            v-model="group"
+        <!-- <v-subheader> Menu </v-subheader> -->
+        <!-- <v-list-item-group
+            v-model="selectedItem"
             active-class="deep-blue--text"
-        >
+        > -->
             <v-list-item
-            
+                v-for="(item, i) in items"
+                :key="i"
             >
-                <v-list-item-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                    <v-list-item-title> 1 </v-list-item-title>
-                </v-list-item-content>
+                <!-- <v-list-item-icon>
+                </v-list-item-icon> -->
+                <!-- <v-list-item-content> -->
+                    <v-list-item-title
+                        v-text="item.text"
+                    ></v-list-item-title>
+                <!-- </v-list-item-content> -->
             </v-list-item>
-        </v-list-item-group>
+        <!-- </v-list-item-group> -->
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -37,7 +41,11 @@
 export default {
     data() {
         return {
-            
+            selectedItem: 0,
+            items: [
+                { text: "Profile", },
+                { text: "Wishlist", },
+            ]
         }
     },
     methods: {
