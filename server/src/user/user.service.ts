d2bb41c4f8d4fb61c0ae7c61cdd6b7bdf1336477
biny <kbpark829@gmail.com>
2021-07-05 21:21:23 +0900

    fix user service using cache, add user controller

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
+
+    async get(key): Promise<any> {
+        return await this.cache.get(key);
+    }
+
+    async set(key, value) {
+        await this.cache.set(key, value, 1000);
+    }
+
+    async reset() {
+        await this.cache.reset();
+    }
+
+    async del(key) {
+        await this.cache.del(key);
+    }
+}
