c05f6e0e392f3ccc6144f16973175adf18e2e4e7
biny <kbpark829@gmail.com>
2021-07-06 16:13:00 +0900

    'registerAs app name' changed

--- server/src/config/steam-api/configuration.ts
+++ server/src/config/steam-api/configuration.ts
@@ -1,5 +1,5 @@
 import { registerAs } from "@nestjs/config";
 
-export default registerAs('redis', () => ({
+export default registerAs('steamAPI', () => ({
     steam_api_key: process.env.STEAM_API_KEY,
 }))
